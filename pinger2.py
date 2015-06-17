e45145085a0cd764c7cfc0015911505ecc3ea7f7
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2015-06-17 13:44:22 +0300

    pinger2 added

---
+++ pinger2.py
@@ -0,0 +1,18 @@
+__author__ = 'ceremcem'
+
+from aktos_dcs import Actor,  ProxyActor, sleep, joinall
+from aktos_dcs.Messages import *
+
+
+class Pinger(Actor):
+    def handle_PingMessage(self, msg):
+        print "Pinger 2 got ping message: ", msg.text, msg.timestamp
+        sleep(2)
+        self.send(PongMessage(text="Hello ponger, this is pinger 2!"))
+
+
+if __name__ == "__main__":
+    ProxyActor()
+    pinger = Pinger()
+    pinger.send(PongMessage(text="startup message from pinger 2..."))
+    joinall([pinger])
