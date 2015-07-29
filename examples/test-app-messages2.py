c8a3f03e8d37649e2e103bfc8d2d32981c402d19
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2015-07-29 23:32:43 +0300

    reorganized examples

--- examples/test-app-messages2.py
+++ examples/test-app-messages2.py
@@ -1,22 +1,17 @@
+import add_import_path # only for examples
 __author__ = 'ceremcem'
 
 from aktos_dcs import *
-
-from aktos_dcs.cca_messages import test
-test()
 
 class TestActor(Actor):
     def handle_TestMessage(self, msg):
         print "received test message: ", msg
 
 if __name__ == "__main__":
+    from aktos_dcs.cca_messages import test
+    test()
+
     ProxyActor()
     test1 = TestActor()
-
-
-    while True:
-        print "test message 1:"
-        test1.send(TestMessage(text="this is test 1!"))
-        sleep(3)
 
     wait_all()
