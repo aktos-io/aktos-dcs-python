d35c0a4d8dbc18b51b25b2a146149a25a8aa7ca3
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2015-08-07 01:59:00 +0300

    color outputs added

--- examples/ponger.py
+++ examples/ponger.py
@@ -6,11 +6,13 @@ from aktos_dcs import *
 class Ponger(Actor):
     def handle_PongMessage(self, msg):
         print "Ponger got pong message:", msg.text, (time.time() - msg.timestamp), msg.debug
-        sleep(1)
-        self.send(PingMessage(text="Hello pinger, this is ponger cca!"))
+        sleep(2)
+        self.send(PingMessage(text="Hello pinger, this is ponger 1!"))
 
 if __name__ == "__main__":
-    ProxyActor(brokers="192.168.2.119:5012:5013", proxy_brokers="localhost:8012:8013")
+    ProxyActor()
+    #ProxyActor(brokers="192.168.2.119:5012:5013", proxy_brokers="localhost:8012:8013")
+    #ProxyActor(brokers="192.168.1.3:5012:5013")
     ponger = Ponger()
     ponger.send(PingMessage(text="startup message from ponger 1..."))
 
