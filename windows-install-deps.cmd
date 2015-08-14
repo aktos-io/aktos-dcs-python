95b97e166722707f58c22607c115e017a2b4398a
ceremcem <ceremcem@ceremcem.net>
2015-08-14 21:18:30 +0300

    Update windows-install-deps.cmd

--- windows-install-deps.cmd
+++ windows-install-deps.cmd
@@ -1,5 +1,5 @@
 @echo off
-C:\Python27\Scripts\easy_install simplejson pyzmq greenlet netifaces
+C:\Python27\Scripts\easy_install ujson pyzmq greenlet netifaces
 C:\Python27\Scripts\pip install gevent 
 echo.
 echo All dependencies are installed...
