525fe52f93173f4afe239cb9b5af9a42beae9dae
Mesut Evin <mesut@mesutevin.net>
2015-10-10 22:52:37 +0300

    Code cleaning..

--- examples/change-brightness.py
+++ examples/change-brightness.py
@@ -6,19 +6,15 @@ from aktos_dcs_lib import *
 class Test(Actor):
     def handle_IoMessage(self, msg):
         msg_body = get_msg_body(msg)
+
         if msg_body["pin_name"] == "izmirhs-brightness":
             with open("/sys/class/backlight/radeon_bl0/brightness", 'w') as f:
                 f.write(msg_body["val"])
+
         print("Got message: %s", msg_body["val"])
 
 
 if __name__ == "__main__":
     ProxyActor(brokers="192.168.2.155:5012:5013")
     Test()
-
-    try:
-        wait_all()
-    except:
-        print "CLEANIN UP!"
-        import RPi.GPIO as g
-        g.cleanup()
+    wait_all()
