682491a71af5c6582aa5f00926ca3ef0da580616
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2015-10-14 10:57:18 +0300

    pypi upload attemt

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'aktos-dcs',
+  packages = ['aktos_dcs'], # this must be the same as the name above
+  version = '0.7',
+  description = 'Aktos DCS core library',
+  author = 'Cerem Cem ASLAN',
+  author_email = 'ceremcem@aktos-elektronik.com',
+  url = 'https://github.com/ceremcem/aktos-dcs', # use the URL to the github repo
+  download_url = 'https://github.com/ceremcem/aktos-dcs/tarball/0.7', # I'll explain this in a second
+  keywords = ['distributed', 'actor', 'concurrent'], # arbitrary keywords
+  classifiers = [],
+)
