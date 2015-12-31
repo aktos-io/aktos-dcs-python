5edd1e81ca3d117f5038f69df081ef550bb50a19
ceremcem <ceremcem@ceremcem.net>
2015-12-31 21:14:07 +0200

    Delete info.py

--- aktos_dcs/info.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'ceremcem'
-#import gevent
-#import color
-#print color.warning("warning: ")
-#print color.warning("warning: You SHOULD define App specific messages ")
-#print color.warning("warning: in AppMessages module in your path")
-#print color.warning("warning: ")
-#gevent.sleep(5)
