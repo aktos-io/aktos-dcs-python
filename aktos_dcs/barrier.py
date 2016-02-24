cc9889ec62e2573020bea2d7b0f2a5d478e4dec1
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2016-02-24 02:14:08 +0200

    new methods are added to barrier class

--- aktos_dcs/barrier.py
+++ aktos_dcs/barrier.py
@@ -2,34 +2,40 @@ __author__ = 'ceremcem'
 
 
 import gevent
+import time
 
 class Barrier(object):
     def __init__(self):
         self.barrier_closed = True
-
-    def wait_answer(self, timeout=None):
-        x = self.wait(timeout)
-        return x
-
-    def go(self):
-        self.answer()
+        self.wait_answer = self.wait
+        self.answer = self.go
 
     def wait(self, timeout=None):
-        t = gevent.Timeout(timeout)
-        success = False
-        try:
-            t.start()
-            self.barrier_closed = True
-            while self.barrier_closed:
-                gevent.sleep(0.0001)
-            success = True
-        except:
-            success = False
-        finally:
-            t.cancel()
+        self.timeout = timeout
+        self.start()
+        self.barrier_closed = True
+        while self.barrier_closed:
+            if self.timeout:
+                if (self.start_time + self.timeout) < time.time():
+                    # timeout!
+                    return False
+            gevent.sleep(0.0001)
 
-        return success
+        return True
 
-    def answer(self):
+    def go(self):
+        if not self.barrier_closed:
+            print "WARNING: BARRIER IS SET TO GO BEFORE IT IS STARTED TO WAIT!!!"
         self.barrier_closed = False
 
+    def start(self):
+        self.restart()
+
+    def restart(self):
+        self.start_time = time.time()
+
+    def add(self, seconds):
+        try:
+            self.timeout += seconds
+        except:
+            self.timeout = seconds
---
+++ examples/barrier_example_2.py
@@ -0,0 +1,46 @@
+import add_import_path # only for examples
+
+from aktos_dcs import *
+import time
+
+class Test(Actor):
+    def prepare(self):
+        self.start_time = time.time()
+
+    def time_diff(self):
+        return time.time() - self.start_time
+
+    def action(self):
+        self.b = Barrier()
+
+        print "waiting forever"
+        success = self.b.wait()
+        print "wait ends", self.time_diff()
+        print "setting again: 3 seconds", self.time_diff()
+        success = self.b.wait(3)
+        print "wait ends", self.time_diff()
+
+
+    def action2(self):
+        sleep(2)
+        print "adding 5 seconds to timeout", self.time_diff()
+        self.b.add(5)
+        sleep(5)
+        print "restarting...", self.time_diff()
+        self.b.restart()
+
+"""
+Expected output:
+
+awaiting forever
+adding 5 seconds to timeout 2.00101590157
+wait ends 5.00072097778
+setting again: 3 seconds 5.00075697899
+restarting... 7.00221204758
+wait ends 10.0028839111
+
+"""
+
+Test()
+wait_all()
+
