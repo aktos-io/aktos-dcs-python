a5af29a3e2de9b0d5fc92ea07eee87ea19dc792f
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2016-03-01 18:13:11 +0200

    cpu usage of Barrier object is decreased

--- aktos_dcs/barrier.py
+++ aktos_dcs/barrier.py
@@ -7,18 +7,17 @@ import time
 
 class Barrier(object):
     def __init__(self, warning=False):
-        self.barrier_closed = True
+        # aliases for functions
         self.wait_answer = self.wait
         self.answer = self.go
+
         self.go_timestamp = 0
         self.tolerance_before_wait = 0.01  # seconds
-        self.warning = warning
+        self.warning = False
         self.barrier_event = Event()
 
     def wait(self, timeout=None):
-        self.timeout = timeout
-        self.start()
-
+        """
         if self.go_timestamp + self.tolerance_before_wait >= time.time():
             if self.warning:
                 print "WARNING: Go signal has been gathered %f seconds before wait, but continuing anyway.." % (
@@ -28,35 +27,20 @@ class Barrier(object):
         else:
             if self.warning:
                 print "WARNING: Go signal has been gathered too long before wait, not continuing!"
-
-        self.barrier_closed = True
-        while True:
-            if self.timeout:
-                if (self.start_time + self.timeout) < time.time():
-                    # timeout!
-                    return False
-            if not self.barrier_closed:
-                break
-
-            gevent.sleep(0.0001)
-
-        return True
+        self.start()
+        """
+        #print "starting event wait, timeout: ", timeout
+        success = self.barrier_event.wait(timeout)
+        #print "finished event wait, success: ", success
+        self.barrier_event.clear()
+        return success
 
     def go(self):
-        if not self.barrier_closed:
-            if self.warning:
-                print "WARNING: BARRIER IS SET TO GO BEFORE IT IS STARTED TO WAIT!!!"
-        self.barrier_closed = False
         self.go_timestamp = time.time()
+        self.barrier_event.set()
 
     def start(self):
-        self.restart()
-
-    def restart(self):
         self.start_time = time.time()
 
-    def add(self, seconds):
-        try:
-            self.timeout += seconds
-        except:
-            self.timeout = seconds
+    def restart(self):
+        self.start()
--- examples/barrier_example_2.py
+++ examples/barrier_example_2.py
@@ -16,19 +16,16 @@ class Test(Actor):
         print "waiting forever"
         success = self.b.wait()
         print "wait ends", self.time_diff()
-        print "setting again: 3 seconds", self.time_diff()
+        #sleep(1)
+        print "setting again to 3 seconds, will timeout", self.time_diff()
         success = self.b.wait(3)
         print "wait ends", self.time_diff()
 
 
     def action2(self):
         sleep(2)
-        print "adding 5 seconds to timeout", self.time_diff()
-        self.b.add(5)
-        sleep(5)
-        print "restarting...", self.time_diff()
-        self.b.restart()
-
+        print "after 2 seconds, go! signal is emitted: ", self.time_diff()
+        self.b.go()
 
     def action3(self):
         x = Barrier()
@@ -39,12 +36,13 @@ class Test(Actor):
 """
 Expected output:
 
-waiting forever
-adding 5 seconds to timeout 2.00101590157
-wait ends 5.00072097778
-setting again: 3 seconds 5.00075697899
-restarting... 7.00221204758
-wait ends 10.0028839111
+Barrier X has been opened before close
+Barrier X continues...
+after 2 seconds, go! signal is emitted:  2.00133919716
+wait ends 2.00144314766
+setting again to 3 seconds, will timeout 2.00145411491
+wait ends 5.00199604034
+
 
 """
 
