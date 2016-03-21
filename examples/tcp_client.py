bb84cb976b0550e13d1223dc887041f839103000
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2016-03-21 23:41:16 +0200

    tcp and udp examples are improved

--- examples/tcp_client.py
+++ examples/tcp_client.py
@@ -4,13 +4,17 @@ def on_receive(data):
     print "received: ", data
 
 #x = TcpClient(host="192.168.2.186", port=23, receiver=on_receive)
-x = TcpClient(host="localhost", port=22334, receiver=on_receive)
+x = TcpClient(host="192.168.2.107", port=22334, receiver=on_receive)
 
 
 try:
     print "Started sending..."
+    i = 0
     while True:
-        x.socket_write("x"*50 + "\n")
+        msg = "hello, I'm tcp client! (%d)" % i
+        print "sending: ", msg
+        x.socket_write(msg + "\n")
+        i += 1
         sleep(1)
 except:
     print "cleaning up..."
--- examples/udp_client.py
+++ examples/udp_client.py
@@ -3,13 +3,17 @@ from aktos_dcs import *
 def on_receive(data):
     print "received: ", data
 
-x = UdpClient(host="localhost", port=5011, receiver=on_receive)
+x = UdpClient(host="192.168.2.107", port=5011, receiver=on_receive)
 
 
 try:
     print "Started sending..."
+    i = 0
     while True:
-        x.socket_write("x"*50 + "\n")
+        msg = "Hello, I'm udp message (%d)" % i
+        print "sending: " + msg
+        x.socket_write(msg + "\n")
+        i += 1
         sleep(1)
 except:
     print "cleaning up..."
