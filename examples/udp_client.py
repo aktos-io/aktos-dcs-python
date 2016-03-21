151fcc6b1e8df286019266f7f8625440f51628da
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2016-03-21 21:40:12 +0200

    udp examples are updated

--- examples/udp_client.py
+++ examples/udp_client.py
@@ -1,3 +1,18 @@
 from aktos_dcs import *
 
-UdpClient()
+def on_receive(data):
+    print "received: ", data
+
+x = UdpClient(host="localhost", port=5011, receiver=on_receive)
+
+
+try:
+    print "Started sending..."
+    while True:
+        x.socket_write("x"*50 + "\n")
+        sleep(1)
+except:
+    print "cleaning up..."
+    x.cleanup()
+
+
--- examples/udp_server.py
+++ examples/udp_server.py
@@ -1,6 +1,24 @@
 from aktos_dcs import *
 
 
-print('Receiving datagrams on :9000')
-UdpServer(':9000').serve_forever()
+class TestHandler(UdpHandlerActor):
+    def on_connect(self):
+        print "there is a connection!"
+
+    def socket_read(self, data):
+        print "I got following data: ", data
+
+    def action(self):
+        i = 0
+        while True:
+            print "sending test data..."
+            self.socket_write("naber... (%d)\n" % i)
+            i += 1
+            sleep(2)
+
+print('Receiving datagrams on :5011')
+#TcpServerActor(address='0.0.0.0', port=22334)
+TcpServerActor(address='0.0.0.0', port=5011, handler=TestHandler)
+wait_all()
+
 
