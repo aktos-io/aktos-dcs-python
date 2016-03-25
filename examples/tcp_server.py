0490e9685b6e409f76707664b4b657a768f7f6c6
cca <cca@aktos1>
2016-03-25 21:47:07 +0200

    tcp_server example is improved

--- examples/tcp_server.py
+++ examples/tcp_server.py
@@ -3,7 +3,10 @@ from aktos_dcs import *
 
 class TestHandler(TcpHandlerActor):
     def on_connect(self):
-        print "there is a connection!"
+        print "there is a connection!", self.client_id
+
+    def on_disconnect(self):
+        print "client disconnected: ", self.client_id
 
     def socket_read(self, data):
         print "I got following data: ", data
