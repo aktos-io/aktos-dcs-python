c229cd7c4e15fd53f1d5244f37f6025368539b43
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2016-04-01 02:45:55 +0300

    serial port actor is added

---
+++ examples/test_micropython.py
@@ -0,0 +1,36 @@
+__author__ = 'ceremcem'
+
+from aktos_dcs import *
+
+import sys
+
+class TestMicropython(SerialPortReader):
+    def prepare(self):
+        print "started reading serial port..."
+        print "-------------------------------"
+        print self.ser
+
+    def send_cmd(self, cmd, s=0.5):
+        self.ser_write(cmd + "\r\n")
+        sleep(s)
+
+    def on_connect(self):
+        print "Connected..."
+        sleep(5)
+        self.send_cmd("""import network""")
+        self.send_cmd("""wlan = network.WLAN(network.STA_IF)""")
+        self.send_cmd("""wlan.active(True)""")
+        self.send_cmd("""wlan.scan()""", 5)
+        self.send_cmd("""wlan.isconnected()""")
+        self.send_cmd("""wlan.connect("aea", "084DA789BF")""", 10)
+        self.send_cmd("""wlan.isconnected()""")
+        self.send_cmd("""wlan.isconnected()""", 2)
+        self.send_cmd("""wlan.isconnected()""", 2)
+        self.send_cmd("""wlan.isconnected()""", 2)
+
+    def serial_read(self, data):
+        #print 'Serial Port Received: ', data
+        sys.stdout.write(data)
+
+TestMicropython(port="/dev/ttyUSB0", baud=115200)
+wait_all()
