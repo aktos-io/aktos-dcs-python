19b04377962069c310a646d7758c2e58a1383664
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2016-04-02 22:41:12 +0300

    standalone application "terminal_emulator.py" is added to examples

--- examples/terminal_emulator.py
+++ examples/terminal_emulator.py
@@ -1,1 +1,33 @@
 __author__ = 'ceremcem'
+
+from aktos_dcs import *
+
+class TerminalEmulator(SerialPortReader):
+    def prepare(self):
+        print "started terminal emulator..."
+        print "-----------------------------"
+        print self.ser
+
+    def action(self):
+        self.start_io_prompt()
+
+    def send_cmd(self, cmd, s=0.5):
+        self.serial_write(cmd + self.line_endings)
+        sleep(0.1)
+
+    def on_disconnect(self):
+        print "\n[[ Device Physically Disconnected... ]]\n"
+
+    def on_connecting(self):
+        while True:
+            print "\n[[ Waiting for Device to be physically connected... ]]\n"
+            sleep(1)
+
+    def on_connect(self):
+        print "\n[[ Device Physically Connected... ]]\n"
+
+        # send any command with `self.send_cmd`
+        ###self.send_cmd("hello")
+
+TerminalEmulator(port="/dev/ttyUSB0", baud=115200)
+wait_all()
