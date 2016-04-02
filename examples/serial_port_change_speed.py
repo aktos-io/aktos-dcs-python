247790684d8e74e28b2490d698185b3be6dcea31
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2016-04-03 01:56:18 +0300

    serial port change speed works automatically on device connect/disconnect

--- examples/serial_port_change_speed.py
+++ examples/serial_port_change_speed.py
@@ -6,11 +6,11 @@ from aktos_dcs import *
 class TestEsp(SerialPortReader):
     def on_connect(self):
         print ""
-        print "[[ Device connected... ]]"
+        print "[[ Device connected, baudrate: %d ]]" % self.ser.baudrate
         print ""
         if self.ser.baudrate != 115200:
-            print "Changing baudrate to 115200"
             self.send_cmd("uart.setup(0,115200,8,0,1)")
+            sleep(0.1)
             self.ser.baudrate = 115200
             self.ser.close()
         else:
@@ -21,6 +21,12 @@ class TestEsp(SerialPortReader):
         print ""
         print "[[ Device disconnected... ]]"
         print ""
+
+    def on_connecting(self):
+        sleep(1)
+        if self.ser.baudrate == 115200:
+            self.ser.baudrate = 9600
+            self.ser.close()
 
     def action(self):
         self.start_io_prompt()
