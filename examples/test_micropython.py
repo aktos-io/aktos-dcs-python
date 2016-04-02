667e240b8247ca620c03d88b12d884eba2f70eef
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2016-04-02 21:24:41 +0300

    serial port send prompt is added

--- examples/test_micropython.py
+++ examples/test_micropython.py
@@ -9,9 +9,11 @@ class TestMicropython(SerialPortReader):
         print "started reading serial port..."
         print "-------------------------------"
         print self.ser
+        self.last_input = None
+        self.line_endings = "\r\n"
 
     def send_cmd(self, cmd, s=0.5):
-        self.serial_write(cmd + "\r\n")
+        self.serial_write(cmd + self.line_endings)
         sleep(s)
 
     def on_disconnect(self):
@@ -38,7 +40,26 @@ class TestMicropython(SerialPortReader):
 
     def serial_read(self, data):
         #print 'Serial Port Received: ', data
-        sys.stdout.write(data)
+        echo_index = 0
+        if self.last_input is not None:
+            if self.last_input[-1] == "\n":
+                self.last_input = self.last_input[:-1]  # remove \n char
+
+            echo_index = data.find(self.last_input)
+            if echo_index > -1:
+                echo_index += len(self.last_input)
+                echo_index += len(self.line_endings)  # remove \r\n at the end
+            self.last_input = None
+        clear_data = data[echo_index:]
+        sys.stdout.write(clear_data)
+        sys.stdout.flush()
+
+    def action2(self):
+        while True:
+            self.last_input = raw_input()
+            self.send_cmd(self.last_input)
+            sleep(0)
+
 
 TestMicropython(port="/dev/ttyUSB0", baud=115200)
 wait_all()
