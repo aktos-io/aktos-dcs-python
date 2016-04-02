b82b82be1dcd3eade1499994023d2efcbe2a837e
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2016-04-02 23:56:44 +0300

    cleaned up

--- examples/test_micropython.py
+++ examples/test_micropython.py
@@ -11,28 +11,24 @@ class TestMicropython(SerialPortReader):
     def action(self):
         self.start_io_prompt()
 
-    def send_cmd(self, cmd, s=0.5):
-        self.serial_write(cmd + self.line_endings)
-        sleep(s)
-
     def on_disconnect(self):
         print "\n[[ Device Physically Disconnected... ]]\n"
 
     def on_connecting(self):
         while True:
             print "\n[[ Waiting for Device to be physically connected... ]]\n"
-            sleep(1)
+            sleep(10)
 
     def on_connect(self):
         print "\n[[ Device Physically Connected... ]]\n"
         sleep(5)
-        self.send_cmd("""import network""")
-        self.send_cmd("""wlan = network.WLAN(network.STA_IF)""")
-        self.send_cmd("""wlan.active(True)""")
+        self.send_cmd("""import network""", 0.5)
+        self.send_cmd("""wlan = network.WLAN(network.STA_IF)""", 0.5)
+        self.send_cmd("""wlan.active(True)""", 0.5)
         self.send_cmd("""wlan.scan()""", 5)
-        self.send_cmd("""wlan.isconnected()""")
+        self.send_cmd("""wlan.isconnected()""", 0.5)
         self.send_cmd("""wlan.connect("aea", "084DA789BF")""", 10)
-        self.send_cmd("""wlan.isconnected()""")
+        self.send_cmd("""wlan.isconnected()""", 2)
         self.send_cmd("""wlan.isconnected()""", 2)
         self.send_cmd("""wlan.isconnected()""", 2)
         self.send_cmd("""wlan.isconnected()""", 2)
