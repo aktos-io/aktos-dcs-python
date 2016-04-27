b154a426c8784da6e2e8a3cd5a3d463fae2a4d83
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2016-04-27 18:25:41 +0300

    fix for windows install

--- install/install-on-windows.cmd
+++ install/install-on-windows.cmd
@@ -3,6 +3,7 @@ echo "Symlinking library"
 mklink /j C:\Python27\Lib\site-packages\aktos_dcs "%~dp0\aktos_dcs"
 echo "installing dependencies"
 C:\Python27\Scripts\pip install -r "%~dp0\requirements.txt"
+C:\Python27\Scripts\pip install "%~dp0\dep\%PROCESSOR_ARCHITECTURE%\*"
 echo.
 echo Finished...
 echo. 
